80a8a139933077f48ed13dbd4776c995e32b92ca
osamareda2002 <osama.reda.abdelghany@gmail.com>
2023-11-23 21:34:08 +0200

    First Version

---
+++ GameZone/Attributes/AllowedExtensionsAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace GameZone.Attributes
+{
+    public class AllowedExtensionsAttribute : ValidationAttribute
+    {
+        private readonly string _allowedExtentions;
+        public AllowedExtensionsAttribute(string allowedExtentions)
+        {
+            _allowedExtentions = allowedExtentions;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file is not null)
+            {
+                var extention = Path.GetExtension(file.FileName);
+                var isAllowed = _allowedExtentions.Split(',').Contains(extention,StringComparer.OrdinalIgnoreCase);
+                if (!isAllowed )
+                    return new ValidationResult($"only {_allowedExtentions} are allowed!");
+            }
+            return ValidationResult.Success;
+
+        }
+    }
+}
---
+++ GameZone/Controllers/GamesController.cs
@@ -0,0 +1,46 @@
+﻿namespace GameZone.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly ICategoriesService _categoriesServices;
+        private readonly IDevicesService _devicesServcies;
+        private readonly IGamesService _gameService;
+        public GamesController(ICategoriesService categoriesServices, IDevicesService devicesServcies, IGamesService gameService)
+        {
+            _categoriesServices = categoriesServices;
+            _devicesServcies = devicesServcies;
+            _gameService = gameService;
+        }
+
+        public IActionResult Index()
+        {
+            var games = _gameService.GetAll();
+            return View(games);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            CreateGameFormViewModel viewModel = new()
+            {
+                Categories = _categoriesServices.GetSelectLists(),
+                Devices = _devicesServcies.GetSelectLists(),
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateGameFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Categories = _categoriesServices.GetSelectLists();
+                model.Devices = _devicesServcies.GetSelectLists();
+                return View(model);
+            }
+
+            await _gameService.Create(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
