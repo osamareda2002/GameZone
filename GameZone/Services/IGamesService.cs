2f7c6561fcf53c80f46d1f11acd6c8571cef0eb8
osamareda2002 <osama.reda.abdelghany@gmail.com>
2023-11-24 16:39:02 +0200

    Final Version

--- GameZone/Services/IGamesService.cs
+++ GameZone/Services/IGamesService.cs
@@ -5,6 +5,9 @@
         IEnumerable<Game> GetAll();
         Game? GetById(int id);
         Task Create(CreateGameFormViewModel model);
+        Task<Game?> Update(EditGameFormViewModel model);
+        bool Delete(int id);
+
 
     }
 }
